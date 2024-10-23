129bd917217cccdfb50e195badae28051e209813
NguyenThaiBinh <nguyenthaibinh838@gmail.com>
2024-10-23 10:31:55 +0700

    Add 5 file laptrinhc

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "shellc1.h"
+#include "shellc2.h"
+
+int main() {
+    print_message1();
+    print_message2();
+    return 0;
+}
---
+++ shell2.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void print_message2() {
+    printf("This is shellc2\n");
+}
